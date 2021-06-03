31739bb4d81de836818cfad23a11e50a3c28303d
Nico Jahn <dev@nicojahn.com>
2021-06-03 21:36:07 +0200

    Added data

--- docker_environment.sh
+++ docker_environment.sh
@@ -3,7 +3,7 @@ image="python:3.9"
 
 container_name="epfd"
 docker stop $container_name
-docker rm $container_name
 
 docker pull $image
-docker run -it --name $container_name -v $(pwd)/:/epfd/ $image bash
+docker run -d --rm -it --name $container_name -v $(pwd)/:/epfd/ $image bash
+docker exec -it $container_name bash -c "cd epfd/EPFD/ && python3 -m pip install -r requirements.txt && python3 -m pip install -e ./PyEnsemble"
