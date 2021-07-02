e89c8ff5307df39198a76d496cf424189c25b0b9
Nico Jahn <dev@nicojahn.com>
2021-07-02 13:58:26 +0000

    Fixed assertion bug and improved docker env

--- docker_environment.sh
+++ docker_environment.sh
@@ -7,7 +7,7 @@ docker stop $container_name
 docker pull $image
 wandb docker run -d --rm -it --name $container_name --dir "/app" $image
 docker exec -it $container_name bash -c "apt update && apt install git jupyter jupyter-nbconvert python3-ipython ffmpeg libsm6 libxext6 -y"
-docker exec -it $container_name bash -c "python3 -m pip install -q -U pip && python3 -m pip install -r requirements.txt && python3 -m pip install -q -U -r ./EPFD/requirements.txt && python3 -m pip install -q -e ./EPFD/PyEnsemble"
+docker exec -it $container_name bash -c "python3 -m pip install -q -U pip && python3 -m pip install -U -r requirements.txt && python3 -m pip install -q -U -r ./EPFD/requirements.txt && python3 -m pip install -q -U -e ./EPFD/PyEnsemble"
 docker exec -it $container_name bash -c "bash convert_notebook_to_script.sh"
 
-#tmux new-session -d 'docker exec -it epfd bash -c "wandb agent nicojahn/htcv/rwr1kwis"'
+#tmux new-session -d 'docker exec -it epfd bash -c "wandb agent nicojahn/htcv/<sweep>"'
